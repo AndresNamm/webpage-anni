ff41fbbca7d1948db697dcc777feb6ca9fc6cf27
Andres Namm <andres.namm.001@gmail.com>
2024-01-15 21:49:59 +0000

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,24 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Anni Pilvar Imetamisnõustaja" />
+        <img src="/boob.jpg" alt="Pilt"  />
+        <Header title="Anni Pilvar" />
+        <Header title="Imetamisnõustaja koduvisiidid Tartus" />
+
+
         <p className="description">
-          Kontakt: 53447089
-          Rohkem infot Facebookis: https://www.facebook.com/imetamisnoustamine/
+          Kontakt: 53447089 
+        </p>
+        <p className="description">
+          E-mail: anni.pilvar@gmail.com
+        </p>
+        <p className="description">
+        Individuaalne imetamisnõustamine rasedatele ja imetavatele emadele.
+        Koduvisiidid Tartus, kokkuleppel Tartumaal.
+        Loenguid ja vestlusringe võimalik tellida üle Eesti.
+        </p>
+        <p className="description">
+          Rohkem infot Facebookis: <a href="https://www.facebook.com/annipilvarimetamisnoustaja">Anni Pilvar Imetamisnõustaja</a> 
         </p>
       </main>
 
